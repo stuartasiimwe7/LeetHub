6a9b4b763ff5d548a4eae14a148b0f704f607fde
Stuart Asiimwe <stuartgabriel@ymail.com>
2024-09-15 05:45:13 +0900

    DONE

--- September/remove_element.py
+++ September/remove_element.py
@@ -1,3 +1,18 @@
+'''
+You're given an array called nums and an integer val. 
+TASK:
+>Go through nums and remove all occurrences of val.  
+>After that, rearrange nums so that the first k elements contain 
+only the values that are not equal to val
+>where k is the count of these remaining elements. 
+
+>Finally, return the value of k
+NB:
+The order of the remaining numbers doesn’t matter, 
+and you don’t need to worry about the leftover spaces in the list. 
+
+'''
+
 class Solution (object):
     def removeElement(self,nums,val):
         k = 0
