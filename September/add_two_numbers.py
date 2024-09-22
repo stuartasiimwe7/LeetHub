f4f0d094d831f05e0c9f2e03dc6eec0631d67089
Stuart Asiimwe <stuartgabriel@ymail.com>
2024-09-23 02:00:28 +0900

    control flow

--- September/add_two_numbers.py
+++ September/add_two_numbers.py
@@ -18,4 +18,12 @@ class Solution(object):
         current = dummy_head
         carry = 0
 
-        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            
